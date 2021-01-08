ec4f7f6c3fdee9c5a601997f3e89560cfb752afa
Jeremy <tan_kai_qun97@yahoo.com.sg>
2021-01-09 03:42:47 +0800

    Add category selector

--- src/components/leaderboard-page/leaderboard-page.tsx
+++ src/components/leaderboard-page/leaderboard-page.tsx
@@ -1,5 +1,26 @@
+import { Link } from "react-router-dom";
+import { Segment, Button } from "semantic-ui-react";
+import { HOME_PATH } from "../../routes/paths";
+import "./leaderboard-page.scss";
+
 function LeaderboardPage() {
-  return <div />;
+  return (
+    <div className="leaderboard-page">
+      <Segment vertical>
+        <Button
+          as={Link}
+          to={HOME_PATH}
+          color="black"
+          content="Back"
+          icon="angle left"
+        />
+      </Segment>
+
+      <Segment vertical textAlign="center">
+        <div />
+      </Segment>
+    </div>
+  );
 }
 
 export default LeaderboardPage;
