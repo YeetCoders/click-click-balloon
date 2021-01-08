2121fe0fd7eff11652cfb7b1f4a834516e079260
kester-ng <kester.ng@visenze.com>
2021-01-09 01:46:50 +0800

    Added api functions

--- functions/wrapper-functions/HelperFunctions.ts
+++ functions/wrapper-functions/HelperFunctions.ts
@@ -1,3 +1,4 @@
+import { count } from 'console';
 import * as admin from 'firebase-admin'
 
 let serviceAccount = require('./spasticballoon-e89ac-firebase-adminsdk-k0a8a-4f72807f03.json');
@@ -7,72 +8,58 @@ admin.initializeApp({
     databaseURL: "https://spasticballoon-e89ac-default-rtdb.firebaseio.com"
 });
 
-export const enterRoom = (roomCode, playerID ,callback) => {
+/**
+ * 
+ * @param playerName String
+ * @param counts int
+ * @param challengeType String -> only these values (30s, 60s and 10s) 
+ */
+export const addPlayerScore = (playerName, counts, challengeType) => {
     var db = admin.database();
-    var ref = db.ref('/rooms/' + String(roomCode)); 
-    
-    // check if room exists
-    ref.on('value', function(snapshot) {
-        if (!snapshot.exists()) {
-            throw new Error("Room does not exist!");
-        }
+    var ref = db.ref('/' + challengeType); 
+
+    let dateTime = new Date()
+
+    ref.push({
+        name: playerName,
+        counts: counts,
+        createdAt: dateTime.toUTCString()
     });
-
-    ref = ref.child(String(playerID))
-
-    ref.set({
-        "count":  0 // set to default 0
-    })
-
-    ref.on('value', callback);
 
     return;
 }
 
-
-export const createRoom = (roomCode, playerID) => {
+export const getChallengeScores = (challengeType) => {
     var db = admin.database();
-    var ref = db.ref("/rooms");
-    ref = ref.child(String(roomCode));
+    var ref = db.ref('/' + challengeType); 
 
-    ref.set({
-        "host": playerID,
-        "started": false
-    })
-
-    return;
-};
-
-export const updateCount = (roomCode, playerID, count) => {
-    var db = admin.database();
-    var ref = db.ref('/rooms/' + String(roomCode)); 
-    
-    // check if room exists
     ref.on('value', function(snapshot) {
         if (!snapshot.exists()) {
-            throw new Error("Room does not exist!");
-        }
-    });
-
-    ref = ref.child(String(playerID))
-    ref.once('value', function(snapshot) {
-        if (!snapshot.exists()) {
-            throw new Error("User not in the room");
+            throw new Error("Challenge type does not exist!");
         } else {
-            ref.set({
-                "count": snapshot.val()["count"] + count
+            let details = snapshot.val();
+            let keys = Object.keys(details);
+            console.log(keys);
+            var result = []
+            console.log(details);
+            for (var i = 0; i < keys.length; i++) {
+                result.push({
+                    "name": details[keys[i]]['name'],
+                    "clickCount": details[keys[i]]['counts'],
+                    "challengeType": challengeType,
+                    "createdAt": details[keys[i]]['createdAt']
+                });
+            }
+            
+            result.sort((a , b) => {
+                if (b['clickCount'] > a['clickCount']) {
+                    return -1
+                } else {
+                    return 1;
+                }
             })
+
+            return result;
         }
-    });
-
-    return;
-}
-
-export const startRoom = (roomCode) => {
-    var db = admin.database();
-    var ref = db.ref('/rooms/' + String(roomCode)); 
-
-    ref.update({
-        "started": true
     });
 }
